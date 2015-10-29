b0f677da0aacce2c0e9235cee5b870c17768662f
aristeuroriz <aristeuroriz@gmail.com>
2015-10-28 23:29:52 -0200

    Initial commit

---
+++ server/services.js
@@ -0,0 +1,6 @@
+ServiceConfiguration.configurations.remove({});
+ServiceConfiguration.configurations.insert({
+	service: 'facebook',
+	appId: '885128941576104',
+	secret: '3b74fa95859bc29053580d39261566f6'
+});
